fb86d19088072ee8389d3573dbe756a06f4b51b7
cjkriska <charleskriska@gmail.com>
2025-05-12 17:04:48 -0500

    begin implementation of profile dashboard, add basic div structure and flex box

--- src/Profile.js
+++ src/Profile.js
@@ -10,42 +10,50 @@ function Profile(props) {
     return (
       <div>
 
-        <div className="profile-container">
-          <div className="container">
-            <div className="profile-info">
-              <div className="profile-avatar">
-                {props.currentUser.imageUrl ? (
-                  <img
-                    src={props.currentUser.imageUrl}
-                    alt={props.currentUser.name}
-                  />
-                ) : (
-                  <div className="text-avatar">
-                    <span>
-                      {props.currentUser.name &&
-                        props.currentUser.name[0]}
-                    </span>
-                  </div>
-                )}
+        {/* <div className="profile-container">
+           <div className="container">
+             <div className="profile-info">
+               <div className="profile-avatar">
+                 {props.currentUser.imageUrl ? (
+                   <img
+                     src={props.currentUser.imageUrl}
+                     alt={props.currentUser.name}
+                   />
+                 ) : (
+                   <div className="text-avatar">
+                     <span>
+                       {props.currentUser.name &&
+                         props.currentUser.name[0]}
+                     </span>
+                   </div>
+                 )}
+               </div>
+               <div className="profile-name">
+                 <h2>{props.currentUser.name}</h2>
+                 <p className="profile-email">{props.currentUser.email}</p>
+               </div>
+             </div>
+           </div>
+         </div> */}
+        <div className="dashboard-container">
+          <div className="dash-left-column">
+            <BiomarkerData />
+          </div>
+          <div className="dash-right-column">
+            <div className="dash-top-row">
+              <HabitTracking />
+            </div>
+            <div className="dash-bottom-row">
+              <div className="dash-bottom-row-left">
+                <Insights />
               </div>
-              <div className="profile-name">
-                <h2>{props.currentUser.name}</h2>
-                <p className="profile-email">{props.currentUser.email}</p>
+              <div className="dash-bottom-row-right">
+                <LearnMore />
               </div>
             </div>
           </div>
         </div>
 
-        <div className="dashboard-container">
-          <div className="dashboard-first-row">
-            <BiomarkerData />
-            <HabitTracking />
-          </div>
-          <div className="dashboard-second-row">
-            <Insights />
-            <LearnMore />
-          </div>
-        </div>
       </div>
     );
 
